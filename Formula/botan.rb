664397fbf2710c7ec2535f864e31ad0d540b7fcb
Can Tunca <cantunca@gmail.com>
2026-04-10 14:48:11 +0300

    Revert "[botan] Update bottles"
    This reverts commit e1c7ee595cf8288e1a61d2fdb411e46613e9a32a.

--- Formula/botan.rb
+++ Formula/botan.rb
@@ -1,32 +1,35 @@
 class Botan < Formula
-  desc "Cryptographic algorithms and formats library in C++"
+  desc "Cryptographic algorithms and formats library in C++ (v2)"
   homepage "https://botan.randombit.net/"
-  url "https://botan.randombit.net/releases/Botan-2.19.5.tar.xz"
-  sha256 "dfeea0e0a6f26d6724c4af01da9a7b88487adb2d81ba7c72fcaf52db522c9ad4"
+  url "https://botan.randombit.net/releases/Botan-2.19.3.tar.xz"
+  sha256 "dae047f399c5a47f087db5d3d9d9e8f11ae4985d14c928d71da1aff801802d55"
   license "BSD-2-Clause"
+  head "https://github.com/randombit/botan.git", branch: "master"
 
-  bottle do
-    rebuild 2
-    sha256 arm64_tahoe:   "dd852d6fe3cc65e68fe497d5582c7f3dbaa4c2afced2acea3a9b0f412042bd82"
-    sha256 arm64_sequoia: "3b52c4abedf724426066b343384e553b586d7a3d69e437a85839ad1adde27868"
-    sha256 arm64_sonoma:  "dc8d1646e46dab1be1f4f53b46f2e111b92610998ec700579c702abe3bee9baf"
-    sha256 sonoma:        "534d9389e8a96ed9c171e1c457c931f70783c94515ff502c6fd876c369666f09"
-    sha256 arm64_linux:   "faf67b146845389405939b4803884a521fd4210e441b078fea28cdeca78e78ac"
-    sha256 x86_64_linux:  "a4d30c4277c84df5b6a0e51b82e049df3bbe90204151143bf68bf214f44cbd86"
+  livecheck do
+    url :homepage
+    regex(/href=.*?Botan[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
-  keg_only :versioned_formula
+  bottle do
+    sha256 arm64_ventura:  "a2ab8e2e8bc1f2f2b0d2c5c9a6e70db7dbf6d8da0f59d7877ae65d0f3e080dac"
+    sha256 arm64_monterey: "f8e217c1f88a3eda865ba873cf7ecde4054a9d03736bc3dbd8e5a447c5ad3750"
+    sha256 arm64_big_sur:  "7af675db93c0d3d31a3fc732640932bdefe5bb871359bfdfef6bf487e9910804"
+    sha256 ventura:        "b29a58fa7e9b7663d5640fd4930e0beb28a14db52cb7e6657f6c087f85fc486c"
+    sha256 monterey:       "6129e49f6502200987dc9f7e0439e1c0ef51a0fe806048ebd6ddce27744f095e"
+    sha256 big_sur:        "580e1e95815e36d9a7faad7201b6f65f3938485bb6c9058c1369fdb37cfee08e"
+    sha256 catalina:       "392dac3351b9e32ceaf9f7e8fa40e6fb5a87be7bb76894f469fd25a7e19ca95e"
+    sha256 x86_64_linux:   "7c5b47bf0f430903a03e71cf065010af7095c30ce8a55421759130a1c11572ce"
+  end
 
-  depends_on "pkgconf" => :build
-  depends_on "python@3.14"
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "python@3.11"
   depends_on "sqlite"
 
   uses_from_macos "bzip2"
   uses_from_macos "zlib"
 
-  def python3
-    which("python3.14")
-  end
+  fails_with gcc: "5"
 
   def install
     ENV.cxx11
@@ -39,7 +42,7 @@ class Botan < Formula
       --with-sqlite3
     ]
 
-    system python3, "configure.py", *args
+    system "python3.11", "configure.py", *args
     system "make", "install"
   end
 
